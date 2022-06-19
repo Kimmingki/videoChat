ba39235fa0fc83161cfa54244d6bda862d93a4b6
Kimmingki <rlaalsrl9569@naver.com>
2022-06-19 16:41:07 +0900

    front setup

--- src/server.js
+++ src/server.js
@@ -1,8 +1,29 @@
 import express from "express";
 
+// -------- variable --------
+
 const app = express();
 const PORT = 4000;
 
+// -------- variable --------
+
+// ------------- middleware ----------------
+
+// 템플릿 설정 및 경로지정
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+// ------------- middleware ----------------
+
+// ------------- routing -----------------
+
+app.get("/", (req, res) => res.render("home"));
+
+// ------------- routing -----------------
+
+// 서버 가동
 app.listen(PORT, () => {
-  console.log(`✅ Listening on port: ${PORT}`);
+  console.log(`✅ Listening on http://localhost:4000`);
 });
