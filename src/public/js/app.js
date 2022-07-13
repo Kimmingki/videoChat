12918260893540d1ebec2fb4f64f6fd9185aabdc
Kimmingki <rlaalsrl9569@naver.com>
2022-07-13 17:25:41 +0900

    메세지 보내기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -10,12 +10,31 @@ const room = document.getElementById("room");
 let roomName = "";
 room.hidden = true;
 
-// function
+// ---------------- Start function part ------------------
+const addMessage = (message) => {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.append(li);
+};
+
+const handleMessageSubmit = (event) => {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("message", input.value, roomName, () =>
+    addMessage(`You: ${value}`)
+  );
+  input.value = "";
+};
+
 const showRoom = () => {
   const h3 = room.querySelector("h3");
+  const form = room.querySelector("form");
   welcome.hidden = true;
   room.hidden = false;
   h3.innerText = `Room ${roomName}`;
+  form.addEventListener("submit", handleMessageSubmit);
 };
 
 const handleRoomSubmit = (event) => {
@@ -25,5 +44,12 @@ const handleRoomSubmit = (event) => {
   roomName = input.value;
   input.value = "";
 };
+// ---------------- End function part ------------------
 
+// event listen
 form.addEventListener("submit", handleRoomSubmit);
+
+// socket event
+socket.on("welcome", () => addMessage("Someone Joined😍"));
+socket.on("bye", () => addMessage("Someone left😭"));
+socket.on("message", addMessage);
--- src/server.js
+++ src/server.js
@@ -2,32 +2,21 @@ import express from "express";
 import http from "http";
 import SocketIO from "socket.io";
 
-// -------- variable --------
-
+// variable
 const app = express();
 const PORT = 4000;
 
-// -------- variable --------
-
-// ------------- middleware ----------------
-
-// 템플릿 설정 및 경로지정
-app.set("view engine", "pug");
+// middleware
+app.set("view engine", "pug"); // 템플릿 설정 및 경로지정
 app.set("views", __dirname + "/views");
 
 app.use("/public", express.static(__dirname + "/public"));
 
-// ------------- middleware ----------------
-
-// ------------- routing -----------------
-
+// routing
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("home"));
 
-// ------------- routing -----------------
-
-const handleListen = () => console.log(`✅ Listening on http://localhost:4000`);
-
+// server 구동
 const server = http.createServer(app);
 const io = SocketIO(server);
 
@@ -36,10 +25,21 @@ const sockets = [];
 
 // socketIO 연결
 io.on("connection", (socket) => {
+  // 채팅방 접속
   socket.on("enter_room", (room, done) => {
     socket.join(room);
+    done();
+    socket.to(room).emit("welcome");
+  });
+  // 채팅방 퇴장
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+  });
+  // 채팅
+  socket.on("message", (msg, room, done) => {
+    socket.to(room).emit("message", msg);
     done();
   });
 });
 
-server.listen(PORT, handleListen);
+server.listen(PORT, () => console.log(`✅ Listening on http://localhost:4000`));
